7acc4fd39a3fd80a8f55e4054956cd57c9d7dabe
Nicolas Froidure <nicolas.froidure@gmail.com>
2018-09-28 09:41:42 +0200

    feat(Main config): Add a warning for unstaged changes

--- src/main/hooks.js
+++ src/main/hooks.js
@@ -5,14 +5,21 @@ if [ "$NODE_ENV" != "cli" ] ; then
   if grep -q '^[0-9]\\+.[0-9]\\+.[0-9]\\+$' "$1" ; then
     exit 0;
   else
-    echo "Please commit with \\\`npm run cz -- (usual commit args)\\\`"
-    echo "To bypass commitizen add \\\`NODE_ENV=cli\\\` to your command"
-    echo "You may want to set an alias: \\\`alias gicz='npm run cz -- '\\\`"
+    echo "⚠️ - Please commit with \\\`npm run cz -- (usual commit args)\\\`"
+    echo "💊 - To bypass commitizen add \\\`NODE_ENV=cli\\\` to your command"
+    echo "💡 - You may want to set an alias: \\\`alias gicz='npm run cz -- '\\\`"
     exit 1;
   fi
 fi`;
+const PRE_COMMIT_CWD_WARNING = `
+if ! git diff-files --quiet --ignore-submodules ; then
+  echo "⚠️ - Unstaged files found:"
+  echo $(git diff-files --shortstat)
+fi`;
 
 module.exports = hooks => {
+  hooks['pre-commit'] = hooks['pre-commit'] || [];
+  hooks['pre-commit'].push(PRE_COMMIT_CWD_WARNING);
   hooks['commit-msg'] = hooks['commit-msg'] || [];
   hooks['commit-msg'].push(COMMIT_MSG_COMMITIZEN_CHECK);
   return hooks;
--- src/main/hooks.test.js
+++ src/main/hooks.test.js
@@ -1,26 +1,11 @@
 'use strict';
 
-const assert = require('assert');
 const hooksTransformer = require('./hooks');
-
-const COMMIT_MSG_COMMITIZEN_CHECK = `
-if [ "$NODE_ENV" != "cli" ] ; then
-  if grep -q '^[0-9]\\+.[0-9]\\+.[0-9]\\+$' "$1" ; then
-    exit 0;
-  else
-    echo "Please commit with \\\`npm run cz -- (usual commit args)\\\`"
-    echo "To bypass commitizen add \\\`NODE_ENV=cli\\\` to your command"
-    echo "You may want to set an alias: \\\`alias gicz='npm run cz -- '\\\`"
-    exit 1;
-  fi
-fi`;
 
 describe('Main', () => {
   describe('Hooks transformer', () => {
     it('should add pre-commit hooks', () => {
-      assert.deepEqual(hooksTransformer({}), {
-        'commit-msg': [COMMIT_MSG_COMMITIZEN_CHECK],
-      });
+      expect(hooksTransformer({})).toMatchSnapshot();
     });
   });
 });
