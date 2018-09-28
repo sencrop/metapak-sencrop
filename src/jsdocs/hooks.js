9e0214bf5050f0c87ce339146c5a78b96c578a99
Nicolas Froidure <nicolas.froidure@gmail.com>
2018-09-28 10:11:06 +0200

    fix(JSDocs config): Ensure the API docs are appended to the README file

--- src/jsdocs/hooks.js
+++ src/jsdocs/hooks.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const PRE_COMMIT_DOC = 'npm run doc && git add API.md';
+// It is safe to run metapak here to add the API
+// to the readme file since it should fail upfront
+// if other changes were detected
+const PRE_COMMIT_DOC = 'npm run doc && npm run metapak && git add README.md';
 
 module.exports = hooks => {
   hooks['pre-commit'] = hooks['pre-commit'] || [];
