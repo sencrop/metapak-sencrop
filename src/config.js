377000f18fe2a3dbee3115865ee7fb6cdf6f3203
Oupsla <nicolas_delperdange@hotmail.com>
2022-05-23 10:54:39 +0200

    feat(node): bump to node 16

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@
 // DRY, set here vars that may be used several times
 // in your metapak module
 module.exports = {
-  lastNodeLTS: '14.16.0',
-  lastNode: '14.16.0',
+  lastNodeLTS: '16.15.0',
+  lastNode: '16.15.0',
 };
